dcf38b2db75365ad37c9973fab6b7b3f11a8e8ce
Nhan Doan <nhandev88@gmail.com>
2023-10-22 11:25:52 +0700

    feat: database storage with Vercel KV

---
+++ src/pages/api/resume.ts
@@ -0,0 +1,24 @@
+import { kv } from '@vercel/kv'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(
+  request: NextApiRequest,
+  response: NextApiResponse,
+) {
+  if (request.method === 'POST') {
+    const result = await kv.set(
+      `resume:${request.query.id as string}`,
+      JSON.stringify(request.body),
+    )
+
+    return result === 'OK'
+      ? response.status(200).json({ message: 'Résumé is updated.' })
+      : response.status(500).json({ message: 'Failed to update résumé.' })
+  }
+
+  const resume = await kv.get(`resume:${request.query.id as string}`)
+
+  return resume
+    ? response.status(200).json(resume)
+    : response.status(400).json({ message: 'Résumé not found.' })
+}
