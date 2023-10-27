5eed984107b5f8b0252aff6627e75de3ca221f93
Nhan Doan <nhandev88@gmail.com>
2023-10-27 16:57:16 +0700

    fix: add missing api

--- src/pages/api/resume.ts
+++ src/pages/api/resume.ts
@@ -5,14 +5,19 @@ export default async function handler(
   request: NextApiRequest,
   response: NextApiResponse,
 ) {
+  const key = `resume:${request.query.id}`
+
   if (request.method === 'POST') {
-    const result = await kv.set(
-      `resume:${request.query.id as string}`,
-      request.body,
-    )
+    const result = await kv.set(key, request.body)
 
     return result === 'OK'
       ? response.status(200).json({ message: 'Résumé has been saved.' })
       : response.status(500).json({ message: 'Failed to save résumé.' })
   }
+
+  const resume = await kv.get(key)
+
+  return resume
+    ? response.status(200).json(resume)
+    : response.status(400).json({ message: 'Résumé not found.' })
 }
