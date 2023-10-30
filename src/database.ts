744d3d25dc4077c0bd0e4138d73b60a56fbe3e86
Nhan Doan <nhandev88@gmail.com>
2023-10-30 21:58:05 +0700

    fix: restore environment variables

--- src/database.ts
+++ src/database.ts
@@ -6,7 +6,15 @@ import {
   PutCommand,
 } from '@aws-sdk/lib-dynamodb'
 
-const docClient = DynamoDBDocumentClient.from(new DynamoDBClient({}))
+const docClient = DynamoDBDocumentClient.from(
+  new DynamoDBClient({
+    region: process.env.AWS_REGION || '',
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
+    },
+  }),
+)
 
 export const getDbItem = (key: string) =>
   docClient.send(
